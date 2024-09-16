b062ace32c0fc62b1f3826d604d98db03d31b0eb
avianshmyerolkar <avinashmyerolkar@gmail.com>
2024-09-16 09:11:03 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='here we will do hands on Versioning of code and data in pipeline',
+    author='avinash-m-yerolkar',
+    license='',
+)
